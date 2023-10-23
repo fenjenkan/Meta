c8c098b3604aac0b40ba8f89c59709871d68ab8e
Felipe Arevalo <fenjenkan@gmail.com>
2023-10-22 18:27:50 -0600

    Primera parte del boseto

---
+++ src/data/services.ts
@@ -0,0 +1,10 @@
+export const services = [{
+    titulo: 'Primer Servicio',
+    descripcion: "esto es una descripcion",
+    imagen: "images/home/servicio1.png"
+},
+{
+    titulo: 'Segundo Servicio',
+    descripcion: "esto es una descripcion segunda",
+    imagen: "images/home/servicio1.png"
+},]
