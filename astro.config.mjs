54a6ac9f51d1b43ba69e5cfbb64518bf1bbf8e9a
Felipe Arevalo <fenjenkan@gmail.com>
2023-10-23 19:44:38 -0600

    algunas configuraciones y tipografia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fenjenkan.github.io',
-    base: '/Meta',
+    output: 'server',
+    adapter: netlify(),
 });
