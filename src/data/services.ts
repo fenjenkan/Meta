495586483cddf034e31cb5781eac3c1e20899709
Felipe Arevalo <fenjenkan@gmail.com>
2023-11-17 19:25:06 -0600

    primera actualizacion de info

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,10 +1,15 @@
 export const services = [{
-    titulo: 'Primer Servicio',
-    descripcion: "esto es una descripcion",
+    titulo: 'Reclutamiento y selección de personal ',
+    descripcion: "Conscientes de la necesidad de tener talento humano de calidad en nuestras empresas, nos encargamos de atraer los perfiles profesionales que más se adaptan a las necesidades y expectativas que tienen nuestros clientes, ejecutando un proceso alineado a las exigencias de cada posición.",
     imagen: "images/home/servicio1.png"
 },
 {
-    titulo: 'Segundo Servicio',
-    descripcion: "esto es una descripcion segunda",
+    titulo: 'Implementación del área de RRHH',
+    descripcion: "Nos encargamos de planear, diseñar y ejecutar todos los procesos del área de recursos humanos, para llevar a cabo una administración eficiente, que les permita a nuestros clientes tomar las mejores decisiones en el desarrollo organizacional de sus empresas. ",
     imagen: "images/home/servicio1.png"
-},]
+},
+{
+    titulo: 'Servicios complementarios de Desarrollo organizacional y RRHH ',
+    descripcion: "La consultoría y el asesoramiento externo nos permite ver desde una visión distinta nuestras áreas de oportunidad. Te apoyamos con servicios complementarios como pruebas psicométricas, estudios socioeconómicos, gestión de actividades de team building, diseño de procesos y procedimiento, entre otros servicios que amplían el alcance del área de recursos humanos de las empresas.",
+    imagen: "images/home/servicio1.png"
+}]
